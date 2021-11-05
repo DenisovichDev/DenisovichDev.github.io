cb07c160de428d810ab3201f2c53888bb8497115
Ivan-Denisovich-py <bhaswarishi2908@gmail.com>
2021-11-05 18:04:46 +0530

    Updating For The username change

--- link-tree/scripts/script.js
+++ link-tree/scripts/script.js
@@ -1,18 +1,18 @@
-const buttons = document.querySelectorAll('li.button');
+const buttons = document.querySelectorAll("li.button");
 const links = [
-    'https://www.instagram.com/bhaswar.py/',
-    'https://twitter.com/DenisovichPy',
-    'https://github.com/ivan-denisovich-py',
-    'mailto:little.denisovich@gmail.com'
-]
+    "https://www.instagram.com/bhaswar.py/",
+    "https://twitter.com/DenisovichPy",
+    "https://github.com/DenisovichDev",
+    "mailto:little.denisovich@gmail.com",
+];
 
 for (let i = 0; i < buttons.length; i++) {
     const b = buttons[i];
 
-    b.addEventListener('click', e => {
-        b.classList.add('big-button');
+    b.addEventListener("click", (e) => {
+        b.classList.add("big-button");
         let icon = b.firstElementChild;
-        icon.setAttribute('fill', 'black');
+        icon.setAttribute("fill", "black");
         window.open(links[i], "_self");
     });
 }
