9c12333299902feaac4b1f1433bd9a381c8e2941
Ivan-Denisovich-py <bhaswarishi2908@gmail.com>
2022-02-06 02:11:20 +0530

    Added fxhash to linktree

--- link-tree/scripts/script.js
+++ link-tree/scripts/script.js
@@ -1,18 +1,19 @@
-const buttons = document.querySelectorAll("li.button");
+const buttons = document.querySelectorAll("li.button")
 const links = [
     "https://www.instagram.com/bhaswar.py/",
     "https://twitter.com/DenisovichPy",
     "https://github.com/DenisovichDev",
     "mailto:little.denisovich@gmail.com",
-];
+    "https://www.fxhash.xyz/u/Denisovich",
+]
 
 for (let i = 0; i < buttons.length; i++) {
-    const b = buttons[i];
+    const b = buttons[i]
 
     b.addEventListener("click", (e) => {
-        b.classList.add("big-button");
-        let icon = b.firstElementChild;
-        icon.setAttribute("fill", "black");
-        window.open(links[i], "_self");
-    });
+        b.classList.add("big-button")
+        let icon = b.firstElementChild
+        icon.setAttribute("fill", "black")
+        window.open(links[i], "_self")
+    })
 }
